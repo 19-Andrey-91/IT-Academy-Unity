caf59f4d3eaea8d46033a4282f46e5805108a41d
19-Andrey-91 <92575746+19-Andrey-91@users.noreply.github.com>
2023-01-26 21:27:59 +0300

    Add ObjectCreator

---
+++ HomeWork/Assets/Scripts/ObjectCreator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCreator : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> prefabs;
+
+    private GameObject instance;
+    private GameObject prefab;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (prefabs == null)
+            {
+                Debug.LogError("Prefab is NULL!!!");
+                return;
+            }
+
+            prefab = prefabs[Random.Range(0, prefabs.Count)];
+
+            if (instance != null)
+            {
+                Destroy(instance);
+            }
+
+            var rotation = Quaternion.identity;
+            var position = new Vector3(Random.Range(-5.0f, 5.0f), 0.5f, Random.Range(-5.0f, 5.0f));
+            instance = Instantiate(prefab, position, rotation);
+        }
+    }
+}
