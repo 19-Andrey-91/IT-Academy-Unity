0ad829736c4fdb790c51e3b42c02f52197a84ce6
19-Andrey-91 <92575746+19-Andrey-91@users.noreply.github.com>
2023-02-22 20:35:01 +0300

    FIxed FlickerLight

--- HomeWork/Assets/Scripts/FlickerLigth.cs
+++ HomeWork/Assets/Scripts/FlickerLigth.cs
@@ -6,22 +6,24 @@ using UnityEngine;
 public class FlickerLigth : MonoBehaviour
 {
     [SerializeField] private GameObject lighting;
-    [SerializeField, Range(0, 100)] private int flickerIntensity;
+    [SerializeField] private float minFlickerSpeed;
+    [SerializeField] private float maxFlickerSpeed;
 
     public bool IsOn { get; set; } = true;
 
-    private void FixedUpdate()
+    private void Update()
     {
-        if(IsOn)
+        StartCoroutine(Flicker());
+    }
+
+    IEnumerator Flicker()
+    {
+        while (IsOn)
         {
-            if(Random.Range(0, 100) < flickerIntensity)
-            {
-                lighting.SetActive(true);
-            }
-            else
-            {
-                lighting.SetActive(false);
-            }
+            lighting.SetActive(true);
+            yield return new WaitForSeconds(Random.Range(minFlickerSpeed, maxFlickerSpeed));
+            lighting.SetActive(false);
+            yield return new WaitForSeconds(Random.Range(minFlickerSpeed, maxFlickerSpeed));
         }
     }
 }
