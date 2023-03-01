e227c0e4e40e15e9b3a590faa49554d2812ce8f2
19-Andrey-91 <92575746+19-Andrey-91@users.noreply.github.com>
2023-03-01 16:39:52 +0300

    Removed unused namespaces

--- HomeWork/Assets/Scripts/Character/CharacterStateIdle.cs
+++ HomeWork/Assets/Scripts/Character/CharacterStateIdle.cs
@@ -1,17 +1,15 @@
-using SpaceCharacterStateMachine;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CharacterStateIdle : CharacterState
+namespace SpaceCharacterStateMachine
 {
-    public CharacterStateIdle(Character character) : base(character)
+    public class CharacterStateIdle : CharacterState
     {
-    }
+        public CharacterStateIdle(Character character) : base(character)
+        {
+        }
 
-    public override void Enter()
-    {
-        
-    }
+        public override void Enter()
+        {
 
+        }
+
+    }
 }
--- HomeWork/Assets/Scripts/Character/CharacterStateRespawn.cs
+++ HomeWork/Assets/Scripts/Character/CharacterStateRespawn.cs
@@ -1,7 +1,4 @@
 using SpaceCharacterStateMachine;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 public class CharacterStateRespawn : CharacterState
 {
